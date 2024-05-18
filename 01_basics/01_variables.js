b30df3e991f2331bfa0dd5c6d66721173c88d2fc
Tejaschore <tejas654321@gmail.com>
2024-05-18 17:22:59 +0530

    learn about variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const id = 1
+let name = "Tejas"
+let lastName = "chore"
+var address = "WARDHA"
+let phoneNo = "7038442611"
+var email 
+
+console.log(id);
+
+console.table([name, lastName, address, phoneNo, email])
