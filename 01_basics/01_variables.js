91e42b844db13fadf7d4e33eb7ea5b28e1b5d499
Tejaschore <tejas654321@gmail.com>
2024-05-18 17:44:20 +0530

    DataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const id = 1
 let name = "Tejas"
 let lastName = "chore"
 var address = "WARDHA"
-let phoneNo = "7038442611"
+let phoneNo = 7038442611
 var email 
 
 console.log(id);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+const age = 18
+
+// primitive dataTypes::
+
+//number
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value 
+// undefined 
+// symbol => unique 
+ 
+
+// Object
+console.log(typeof "Tejas")
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined);
