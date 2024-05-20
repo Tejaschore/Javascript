358560f62aa7b4e927d7d990f946a3430076b3e7
Tejaschore <tejas654321@gmail.com>
2024-05-20 18:18:30 +0530

    learn conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "15"
+console.log(typeof score);
+
+let s1 = Number(score)
+console.log(typeof s1);
+
+let name = "tejas"
+let name2 = Number(name)
+console.log(typeof name2);
+console.log(name2);
+
+let value = false //"true"
+let v1 = Number(value)
+
+console.log(typeof v1)
+console.log(v1);
+
+let v2 = true
+let v3 = String(v2)
+console.log(typeof v3)
+console.log(v3);
