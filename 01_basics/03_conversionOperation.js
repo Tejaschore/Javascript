d06a4b56f74af0e311b0440f31a4e48825a57e31
Tejaschore <tejas654321@gmail.com>
2024-05-20 18:21:39 +0530

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,26 @@
-let score = "15"
-console.log(typeof score);
+// let score = "15"
+// console.log(typeof score);
 
-let s1 = Number(score)
-console.log(typeof s1);
+// let s1 = Number(score)
+// console.log(typeof s1);
 
 let name = "tejas"
 let name2 = Number(name)
+let n1= Boolean(name)
 console.log(typeof name2);
 console.log(name2);
 
-let value = false //"true"
-let v1 = Number(value)
+console.log(typeof n1);
+console.log(n1);
 
-console.log(typeof v1)
-console.log(v1);
+// let value = false //"true"
+// let v1 = Number(value)
 
-let v2 = true
-let v3 = String(v2)
-console.log(typeof v3)
-console.log(v3);
+// console.log(typeof v1)
+// console.log(v1);
+
+// let v2 = true
+// let v3 = String(v2)
+// console.log(typeof v3)
+// console.log(v3);
+
