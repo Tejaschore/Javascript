796cf29158ed7bd90c82187026752c1226b11cfa
Tejaschore <tejas654321@gmail.com>
2024-05-22 19:23:15 +0530

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,14 +4,14 @@
 // let s1 = Number(score)
 // console.log(typeof s1);
 
-let name = "tejas"
-let name2 = Number(name)
-let n1= Boolean(name)
-console.log(typeof name2);
-console.log(name2);
+// let name = "tejas"
+// let name2 = Number(name)
+// let n1= Boolean(name)
+// console.log(typeof name2);
+// console.log(name2);
 
-console.log(typeof n1);
-console.log(n1);
+// console.log(typeof n1);
+// console.log(n1);
 
 // let value = false //"true"
 // let v1 = Number(value)
@@ -24,3 +24,33 @@ console.log(n1);
 // console.log(typeof v3)
 // console.log(v3);
 
+
+
+// *************************Operations**************************
+
+// console.log(1+2)
+// console.log(1-2)
+// console.log(1*2)
+// console.log(1/2)
+// console.log(1%2)
+
+
+// let str1 = "hello"
+// let str2 = " Tejas"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 +"5");
+// console.log("1" + 5);
+// console.log("1" + 2 + 5);
+// console.log(1 + 2 + "5"); //changes
+
+// let n1 , n2 , n3
+// n1 = n2 = n3 = 2+2
+// console.log(n2);
+
+
+let counter = 100
+// counter++
+++counter
+console.log(counter);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // convert string to compare with 1
+// console.log("02" > 3); // same data type 
+
+// console.log(null > 0);
+
+// console.log(null== 0);
+
+// console.log(null >= 0); // convert null into 0 then compare it with 0
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" ==2);
+console.log("2" === 2);
