1921ddfac4c0d342a6d3a55eeef1b9e02119edd8
Tejaschore <tejas654321@gmail.com>
2024-05-22 19:22:13 +0530

    learn about string

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,17 @@
+const name = "tejas"
+const n = 50
+// console.log(name + n);
+
+// console.log(`my name is ${name} and my repocount is ${n}`);
+
+ const gameName = new String('tejas01_chore_24')
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+const newString = "   tejas    "
+console.log(newString);
+console.log(newString.trim());
+
+console.log(gameName.split('_'));
