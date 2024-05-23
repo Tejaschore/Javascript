da491f1b502c825bac58f783ac0b6deb44369ad5
Tejaschore <tejas654321@gmail.com>
2024-05-23 16:56:09 +0530

    math and nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const n1=100
+// console.log(n1);
+
+const n2 = new Number(122)
+console.log(n2);
+
+const num = 444.0000
+// console.log(num.toString.length);
+// console.log(num.toFixed(2));
+
+const num2 = 22.845
+// console.log(num2.toPrecision(2));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++maths+++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5,2,4,8,3));
+// console.log(Math.max(7,5,2,0,8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max - min +1)) + min);
