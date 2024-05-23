9e8c2f6f78a58773b7110bc043b0e8263a52ce02
Tejaschore <tejas654321@gmail.com>
2024-05-23 17:15:41 +0530

    Dates in Js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,10 @@
+const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+const newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1); // in js date starts with zero
+console.log(newDate.getDay()+1);
