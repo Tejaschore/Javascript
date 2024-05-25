e25367fd064593012435180d08061348d807d579
Tejaschore <tejas654321@gmail.com>
2024-05-25 18:33:03 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+const arr = [4,2,9,8]
+const str = ["tejas", "om" , "sam"]
+const a1 = new Array(1,2,3,4,5)
+
+// console.log(arr[2]);
+// console.log(str[0]);
+
+//Array methods
+
+// arr.push(6)
+// arr.push(7)
+
+// console.log(arr);
+// arr.pop()
+
+
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(2));
+
+
+// const newarr = arr.join()
+// console.log(arr);
+// console.log(newarr); // array convert into string
+
+const n1 = arr.slice(1,3)
+console.log("a", arr);
+console.log(n1);
+
+const n2 = arr.splice(1,3)
+console.log("b",arr);
+console.log(n2);
