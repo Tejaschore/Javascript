dc4360f1e7ea796516a82fc39fc27f7bbc3ff550
Tejaschore <tejas654321@gmail.com>
2024-06-01 14:29:36 +0530

    commit 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -16,7 +16,21 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.email);
-console.log(regularUser.fullName);
-console.log(regularUser.fullName.userFullName);
-console.log(regularUser.fullName.userFullName.firstName);
+// console.log(regularUser.email);
+// console.log(regularUser.fullName);
+// console.log(regularUser.fullName.userFullName);
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj3 =  Object.assign({},obj1,obj2) //{} target and source
+// const obj3 = {obj1,obj2}
+// console.log(obj3);
+
+const obj4 = {...obj1,...obj2}
+// console.log(obj4);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
