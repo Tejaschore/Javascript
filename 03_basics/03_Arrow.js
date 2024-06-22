a4331c43745bf9a8d931463df1ab41a0f8d3003c
Tejaschore <tejas654321@gmail.com>
2024-06-22 21:36:35 +0530

    used Arrow function

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -8,6 +8,27 @@ const user = {
     }
 
 }
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// function add(){
+//     let usernamne = "tejas"
+//     console.log(this.usernamne);
+// }
+// add()
+
+// const add1 = function (){
+//     let usernamne = "tejas"
+//     console.log(this.usernamne);
+// }
+// add1()
+
+
+// Arrow function
+
+ const add1 = () => {
+        let usernamne = "tejas"
+        console.log(this.usernamne);
+    }
+    add1()
