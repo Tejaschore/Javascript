cfe38c6247217b2d3bc6332ddcca46e606e3ae8d
Tejaschore <tejas654321@gmail.com>
2024-07-26 17:50:36 +0530

    if else statement

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,10 +2,19 @@
 //if
 
 if (2 == "2"){
-    console.log("executed");
+    // console.log("executed");
 }
 
-const temp = 41
+const temp = 58
 if(temp < 50){
     console.log("executed2");
+}
+else{
+    console.log("temp is greater than 50");
+}
+
+const score = 200
+if(score > 100){
+    const power = "fly"
+    console.log(`user power:${power}`);
 }
