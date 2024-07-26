bcca53b17634859835cadd5c2eb031da31b54314
Tejaschore <tejas654321@gmail.com>
2024-07-26 17:53:28 +0530

    external scope

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,20 +1,23 @@
 
 //if
 
-if (2 == "2"){
-    // console.log("executed");
-}
+// if (2 == "2"){
+//     // console.log("executed");
+// }
 
-const temp = 58
-if(temp < 50){
-    console.log("executed2");
-}
-else{
-    console.log("temp is greater than 50");
-}
+// const temp = 58
+// if(temp < 50){
+//     console.log("executed2");
+// }
+// else{
+//     console.log("temp is greater than 50");
+// }
 
-const score = 200
-if(score > 100){
-    const power = "fly"
-    console.log(`user power:${power}`);
-}
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power:${power}`);
+// }
+
+const balance = 1000
+if(balance > 500) console.log("test");
