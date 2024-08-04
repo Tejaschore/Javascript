7f0e881c0ad95a45979b21c7684fede7b657f62f
Tejaschore <tejas654321@gmail.com>
2024-08-04 14:09:20 +0530

    updated

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,6 +1,13 @@
 
 const myNums = [1,2,3,4,5,6,7,8,9]
 
-const newNums = myNums.filter( (nums) => nums >= 5)
+// const newNums = myNums.filter( (nums) => nums >= 5)
+
+// console.log(newNums);
+
+const newNums = myNums.filter( (nums) => {
+    return nums >= 5
+})
+
 
 console.log(newNums);
