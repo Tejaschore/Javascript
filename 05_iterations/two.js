913674aedda6ed156be7a19f1eafd3184cfa73a9
Tejaschore <tejas654321@gmail.com>
2024-08-04 13:13:12 +0530

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// while loop
+
+let index = 0
+
+while (index <=10) {
+    console.log(`value of index is${index}`);
+
+    index=index + 2
+    
+}
+
+let myArray = ["Tejas" , "Om" , "Sham"]
+let a = 0
+while (a <myArray.length) {
+    console.log(`value is ${myArray[a]}`);
+    a = a +1
+    
+}
